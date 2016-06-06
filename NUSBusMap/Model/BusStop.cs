03b0cd9b4d107ff9c27d7679b299bf96e46137a1
landuomu <lhchchc@gmail.com>
2016-06-06 18:20:49 +0800

    create model for bus stops and bus services

---
+++ NUSBusMap/Model/BusStop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NUSBusMap
+{
+	public class BusStop
+	{
+		public int busStopCode { get; set; }
+		public double latitude { get; set; }
+		public double longitude { get; set; }
+		public string name { get; set; }
+		public List<BusSvc> services { get; set; }
+	}
+}
+
---
+++ NUSBusMap/Model/BusSvc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NUSBusMap
+{
+	public class BusSvc
+	{
+		public string routeNo { get; set; }
+		public List<BusStop> stops { get; set; }
+	}
+}
+
