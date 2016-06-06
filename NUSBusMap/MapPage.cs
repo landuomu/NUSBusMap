abf4102dc7f5dd1b67eea0fd9cbcc6e4e7f98d89
nusbusmap <e0011792@comp.nus.edu.sg>
2016-06-06 16:23:58 +0800

    Add sample bus stops on map as pins

--- NUSBusMap/MapPage.cs
+++ NUSBusMap/MapPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -15,8 +16,25 @@ namespace NUSBusMap
 	                WidthRequest = 960,
 	                VerticalOptions = LayoutOptions.FillAndExpand
 	            };
-	        var stack = new StackLayout { Spacing = 0 };
-	        stack.Children.Add(map);
+
+	        // pins for bus stops
+			// var position = new Position(37,-122); // Latitude, Longitude
+			// testing list of bus stops
+			List<Position> busStops = new List<Position>() {
+				new Position (1.29630305719228, 103.78341318580553), // NUH
+				new Position (1.293383, 103.784394) // Kent Ridge MRT Station
+			};
+			// busStops.Add(new Position (1.29630305719228, 103.78341318580553));
+			foreach (Position position in busStops) {
+				// var position = new Position (1.29630305719228, 103.78341318580553);
+				var pin = new Pin {
+				            Type = PinType.Place,
+				            Position = position,
+				            Label = "Bus stop name - Bus stop code",
+							Address = "Bus service - Bus arrival timings"
+				        };
+				map.Pins.Add(pin);
+			}
 
 	        // slider to change radius from 0.1 - 0.9 km
 			var slider = new Slider (1, 9, 5);
@@ -26,7 +44,12 @@ namespace NUSBusMap
 			    map.MoveToRegion(MapSpan.FromCenterAndRadius(
 			    	map.VisibleRegion.Center, Distance.FromKilometers(radius)));
 			};
-			stack.Children.Add (slider);
+
+
+			// add map and slider to stack layout
+	        var stack = new StackLayout { Spacing = 0 };
+	        stack.Children.Add(map);
+			stack.Children.Add(slider);
 
 	        Content = stack;
 	    }
