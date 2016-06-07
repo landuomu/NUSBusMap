85e5e6d0cc0033969cf1959a42e3a7acfebfb996
landuomu <lhchchc@gmail.com>
2016-06-07 17:31:38 +0800

    add functionality to get current location (cannot test on simulator, need to test on device)

--- NUSBusMap/MapPage.cs
+++ NUSBusMap/MapPage.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+
+using XLabs.Platform.Device;
+using XLabs.Platform;
+using XLabs.Ioc;
+using XLabs.Platform.Services.Geolocation;
 
 namespace NUSBusMap
 {
@@ -13,7 +21,7 @@ namespace NUSBusMap
 	    	// map with default centre at NUS
 	        map = new Map(
 	            MapSpan.FromCenterAndRadius(
-	                    new Position(1.2966,103.7764), Distance.FromKilometers(0.5))) {
+	                    new Xamarin.Forms.Maps.Position(1.2966,103.7764), Distance.FromKilometers(0.5))) {
 	                IsShowingUser = true,
 	                HeightRequest = 100,
 	                WidthRequest = 960,
@@ -32,7 +40,7 @@ namespace NUSBusMap
 			foreach (BusStop busStop in busStops) {
 				var pin = new Pin {
 				            Type = PinType.Place,
-				            Position = new Position(busStop.latitude, busStop.longitude),
+							Position = new Xamarin.Forms.Maps.Position(busStop.latitude, busStop.longitude),
 				            Label = busStop.name + " - " + busStop.busStopCode,
 							Address = "Bus service - Bus arrival timings"
 				        };
@@ -48,13 +56,63 @@ namespace NUSBusMap
 			    	map.VisibleRegion.Center, Distance.FromKilometers(radius)));
 			};
 
+			// temp button to test geolocation
+			Button geoButton = new Button {
+				Text = "Get current location"
+			};
+			geoButton.Clicked += OnGetLocation;
 
 			// add map and slider to stack layout
 	        var stack = new StackLayout { Spacing = 0 };
 	        stack.Children.Add(map);
 			stack.Children.Add(slider);
+			stack.Children.Add(geoButton);
 
 	        Content = stack;
+
+	    }
+
+	    private void OnGetLocation (object sender, EventArgs e) {
+			var geolocation = GetCurrentPosition ().ContinueWith(t => {
+	            if (t.IsFaulted)
+	            {
+	                System.Diagnostics.Debug.WriteLine("Error : {0}", ((GeolocationException)t.Exception.InnerException).Error.ToString());
+	            }
+	            else if (t.IsCanceled)
+	            {
+	                System.Diagnostics.Debug.WriteLine("Error : The geolocation has got canceled !");
+	            }
+	            else
+	            {
+	                var currentLocation = new Xamarin.Forms.Maps.Position (t.Result.Latitude, t.Result.Longitude);
+
+	                map.MoveToRegion(MapSpan.FromCenterAndRadius(currentLocation, Distance.FromKilometers(0.5)));
+	            }
+	        }, TaskScheduler.FromCurrentSynchronizationContext ());
+	    }
+
+		public async Task<XLabs.Platform.Services.Geolocation.Position> GetCurrentPosition() {
+			IGeolocator geolocator = Resolver.Resolve<IGeolocator>();
+
+			XLabs.Platform.Services.Geolocation.Position result = null;
+
+	        if (geolocator.IsGeolocationEnabled) {
+	            try {
+	                if (!geolocator.IsListening)
+	                    geolocator.StartListening(1000, 1000);
+
+	                var task = await geolocator.GetPositionAsync(10000);
+
+	                result = task;
+
+	                System.Diagnostics.Debug.WriteLine("[GetPosition] Lat. : {0} / Long. : {1}", result.Latitude.ToString("N4"), result.Longitude.ToString("N4"));
+	            }
+	            catch (Exception e) {
+	                System.Diagnostics.Debug.WriteLine ("Error : {0}", e);
+	            }
+	        }
+
+	        return result;
 	    }
 	}
 }
