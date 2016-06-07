f884fe8037594ede27f517909e3ddd4f5a0e18fb
landuomu <lhchchc@gmail.com>
2016-06-07 16:31:04 +0800

    add xlabs forms for cross-platform geolocation support

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,6 +11,12 @@ using Android.OS;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
+// New Xlabs
+using XLabs.Ioc; // Using for SimpleContainer
+using XLabs.Platform.Services.Geolocation; // Using for Geolocation 
+using XLabs.Platform.Device; // Using for Display
+// End new Xlabs
+
 namespace NUSBusMap.Droid
 {
 	[Activity (Label = "NUSBusMap.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -20,12 +26,18 @@ namespace NUSBusMap.Droid
 		{
 			base.OnCreate (bundle);
 
+			// init json stream loader
 			JsonLoader.Loader = new StreamLoader (this);
 
+			// init Xlabs for geolocator
+			var container = new SimpleContainer();
+			container.Register<IDevice> (t => AndroidDevice.CurrentDevice); 
+			container.Register<IGeolocator, Geolocator>(); 
+			Resolver.SetResolver(container.GetResolver()); // Resolving the services
+
+			// init xamarin forms and maps
 			global::Xamarin.Forms.Forms.Init (this, bundle);
-
 			Xamarin.FormsMaps.Init (this, bundle);
-
 			LoadApplication (new App ());
 		}
 	}
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+using XLabs.Ioc; // Using for SimpleContainer
+using XLabs.Platform.Services.Geolocation; // Using for Geolocation 
+using XLabs.Platform.Device; // Using for Device
+
 namespace NUSBusMap.iOS
 {
 	[Register ("AppDelegate")]
@@ -12,14 +16,19 @@ namespace NUSBusMap.iOS
 	{
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			// init json stream loader
 			JsonLoader.Loader = new StreamLoader ();
 
+			// init xlabs for geolocator
+			var container = new XLabs.Ioc.SimpleContainer(); // Create SimpleCOntainer
+			container.Register<IDevice>(t => AppleDevice.CurrentDevice); // Register Device
+			container.Register<IGeolocator, Geolocator>(); // Register Geolocator
+			Resolver.SetResolver(container.GetResolver()); // Resolving the services
+
+			// init xamarin forms and maps
 			global::Xamarin.Forms.Forms.Init ();
-
 			Xamarin.FormsMaps.Init();
-
 			LoadApplication (new App ());
-
 			return base.FinishedLaunching (app, options);
 		}
 	}
