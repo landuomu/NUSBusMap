f60eded01b28ccbc10709b9fb057daddb0c9ab2b
landuomu <lhchchc@gmail.com>
2016-06-14 13:22:57 +0800

    Add BusOnRoad model and add/remove methods to keep track of buses on road

--- NUSBusMap/BusHelper.cs
+++ NUSBusMap/BusHelper.cs
@@ -7,16 +7,39 @@ namespace NUSBusMap
 	{
 		public static Dictionary<int,BusStop> BusStops;
 		public static Dictionary<string,BusSvc> BusSvcs;
+		public static Dictionary<string,BusOnRoad> ActiveBuses;
 
 		public static void LoadBusData ()
 		{
 			BusStops = JsonLoader.LoadStops();
 			BusSvcs = JsonLoader.LoadSvcs();
+			ActiveBuses = new List<BusOnRoad> ();
 		}
 
 		// TODO: helper methods to extract specific bus data
 
+		// add bus when bus starts to ply on road
+		public static bool AddBusOnRoad (string vehiclePlate, string routeName) {
+			var svc = BusSvcs[routeName];
+			var stopEnum = svc.stops.GetEnumerator();
+			stopEnum.MoveNext ();
 
+			var bus = new BusOnRoad {
+				vehiclePlate = vehiclePlate,
+				routeName = routeName,
+				firstStop = svc.firstStop,
+				lastStop = svc.lastStop,
+				nextStopEnumerator = stopEnum,
+				// take firstStop position as initial bus position
+				latitude = BusStops[svc.firstStop].latitude,
+				longitude = BusStops[svc.firstStop].longitude
+			};
+			return ActiveBuses.Add (vehiclePlate, bus);
+		}
+
+		public static bool RemoveBusOnRoad (string vehiclePlate) {
+			return ActiveBuses.Remove (vehiclePlate);
+		}
 	}
 }
 
---
+++ NUSBusMap/Model/BusOnRoad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace NUSBusMap
+{
+	public class BusOnRoad
+	{
+		public string vehiclePlate { get; set; }
+		public string routeName { get; set; }
+		public int firstStop { get; set; }
+		public int lastStop { get; set; }
+		public IEnumerator nextStopEnumerator { get; set; }
+		public double latitude { get; set; }
+		public double longitude { get; set; }
+	}
+}
+
