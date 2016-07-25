45e5168c8ecd4a94e0e1c0038849659aadd2d977
nusbusmap <e0011792@comp.nus.edu.sg>
2016-07-26 04:31:30 +0800

    Add SvcInfoPage content

---
+++ NUSBusMap/Misc/BusStopCell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NUSBusMap
+{
+    public class BusStopCell : ViewCell
+    {
+        public BusStopCell(string busStopCode)
+        {
+			var grid = new Grid {
+			    ColumnSpacing = 0,
+			    RowSpacing = 0,
+			    Padding = new Thickness(10),
+			    RowDefinitions = { new RowDefinition { Height = new GridLength(10, GridUnitType.Star) } },
+			    ColumnDefinitions = {
+			        new ColumnDefinition { Width = new GridLength(5, GridUnitType.Star) },
+			        new ColumnDefinition { Width = new GridLength(5, GridUnitType.Star) },
+			    }
+			};
+
+            Label busStopName = new Label
+            {
+                Text = BusHelper.BusStops[busStopCode].name,
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Label roadName = new Label
+            {
+                Text = BusHelper.BusStops[busStopCode].road,
+                TextColor = Color.Gray,
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            grid.Children.Add(busStopName,0,0);
+            grid.Children.Add(roadName,1,0);
+            View = grid;
+        }
+    }
+}
--- NUSBusMap/Page/SvcInfoPage.cs
+++ NUSBusMap/Page/SvcInfoPage.cs
@@ -6,11 +6,42 @@ namespace NUSBusMap
 {
 	public class SvcInfoPage : ContentPage
 	{
-		public SvcInfoPage (string routeName)
+		public SvcInfoPage (string busSvcName)
 		{
-			Title = routeName + " Route Information";
-			Content = new Label { Text = "Page under construction" };
-		}
+			Title = busSvcName + " Route Information";
+
+			// show only for nus buses
+			if (!BusHelper.IsPublic (busSvcName)) {
+				Label header = new Label {
+					Text = busSvcName,
+					FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+					FontAttributes = FontAttributes.Bold,
+					HorizontalOptions = LayoutOptions.Center
+				};
+
+				var view = new TableView () {
+					Intent = TableIntent.Data
+				};
+				var root = new TableRoot ();
+				var section = new TableSection ();
+
+				foreach (string busStopCode in BusHelper.BusSvcs[busSvcName].stops) {
+					section.Add (new BusStopCell (busStopCode));
+				}
+
+				root.Add (section);
+				view.Root = root;
+
+				Content = new StackLayout {
+					Children = {
+						header,
+						view
+					}
+				};
+			} else {
+				Content = new Label { Text = "Page under construction" };
+			}
+        }
 	}
 }
 
